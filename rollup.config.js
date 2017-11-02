74ece845bdc0aedcdaa7a60698678548a2e8d4eb
Cornerjob <cornerjob@Cornerjobs-MacBook-Pro.local>
2017-11-02 18:53:51 +0100

    Add readme, Update developement environment

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,21 @@ import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 import uglify from 'rollup-plugin-uglify';
 
+const isDev = process.env.ENVIRONMENT === 'dev';
 const format = process.env.FORMAT;
 const name = format === 'umd' && 'NewRelicHostAppsBundle';
+const fileName = isDev ? 'NewRelicHostApps.js' : 'NewRelicHostApps.min.js';
 
 export default {
   input: 'src/index.js',
   name,
   output: {
-    file: `dist/${format}/NewRelicHostApps.min.js`,
+    file: `dist/${format}/${fileName}`,
     format,
   },
   plugins: [
     eslint(),
     babel(),
-    uglify(),
+    !isDev && uglify(),
   ],
 };
